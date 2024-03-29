7ab986da250d760d9ef8aaa152ff7f0ba4dff886
stella <stellanwachukwu42@gmail.com>
2024-03-29 16:20:11 -0700

    all project files

---
+++ validate.php
@@ -0,0 +1,11 @@
+<?php
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$DOB = $_POST['date of birth'];
+$Address= $_POST['address'];
+$Telnumber= $_POST['phonenum'];
+$Gender= $_POST['gender'];
+$areyouready = $_POST['ready']
+
+echo "$fullname<br>$email"
+?>
